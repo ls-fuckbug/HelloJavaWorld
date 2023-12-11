649a4815a8618444aa001652331e71dfa497482f
liushuai <83873957@qq.com>
2023-12-11 23:07:34 +0800

    update

--- HelloWorld/src/main/java/com/superlittlesam/App.java
+++ HelloWorld/src/main/java/com/superlittlesam/App.java
@@ -1,13 +1,32 @@
 package com.superlittlesam;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import cn.hutool.core.util.StrUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    /**
+     * Hello，World
+     *
+     * @param who 参数，非必须
+     * @return Hello, ${who}
+     */
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (StrUtil.isBlank(who)) {
+            who = "World";
+        }
+        return StrUtil.format("Hello, {}!", who);
     }
 }
